f6bc0f4e8265fbca938ab22365851e44063f45bf
charitreddy1 <charit.reddy@nineleaps.com>
2019-08-11 22:51:25 +0530

    post

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -1,21 +1,65 @@
 import React, {Component} from 'react';
-import { Button, Divider, Header, Grid } from 'semantic-ui-react';
+import { Button, Divider, Header, Grid, Icon } from 'semantic-ui-react';
 import 'semantic-ui/dist/semantic.min.css';
 import {Image} from 'semantic-ui-react';
 import GoogleLogin from 'react-google-login'
 import axios from 'axios'
 
 
-
-const responseGoogle= (response) => {
-  console.log(response.profileObj.email)
-  var email=response.profileObj.email
-  axios.post('https://emailtest.free.beeceptor.com',email)
-
-}
+//Response sent by Google after successful login.
+// const responseGoogle= (response) => {
+//   console.log(response.profileObj)
+//   var email=response.profileObj.email;
+//   var adminDetails = {};
+//   adminDetails['email'] =  email;
+//   axios.post('https://1e882d3c.ngrok.io/admin/email',adminDetails).then(response=>
+//   {   
+//       alert(JSON.stringify(response.data));
+//       //this.setState({status : response.data[0].status })
+//       alert(response.data[0].status);
+//       if(response.data[0].status==1)
+//       {
+        
+//       }
+//   })
+//   alert(JSON.stringify(adminDetails));
+//   console.log(adminDetails);
+// }
 
 class HomePage extends Component{
+  constructor(props){
+  super(props);
+  this.responseGoogle=this.responseGoogle.bind(this);
+}
+
+  responseGoogle= (response) => {
+    console.log(response.profileObj)
+    var email=response.profileObj.email;
+    var adminDetails = {};
+    adminDetails['email'] =  email;
+    axios.post('https://1e882d3c.ngrok.io/admin/email',adminDetails).then(response=>
+    {   
+        alert(JSON.stringify(response.data));
+        this.setState({status : response.data[0].status })
+        alert('State Alert:'+this.state.status);
+        
+        if(this.state.status==1)
+                    {
+                      this.props.history.push({
+                          pathname: '/VendorHome',
+                          state : {email : this.state.vmail}
+                      }) 
+                    }
+          else
+                    {
+                     alert("Unauthorized for Admin Acces! Please request Super Admin.")
+                    } 
+               
   
+    })
+    alert(JSON.stringify(adminDetails));
+    console.log(adminDetails);
+  }
     render(){
         return(
           
@@ -36,11 +80,11 @@ class HomePage extends Component{
                         clientSecret="_6RU-J34o06HkJDA4CF6MHqQ"
                         hostedDomain="nineleaps.com"
                         render={renderProps => (
-                        <Button style={{marginTop:"100%",marginRight:"0%"}} color="blue" size="massive" onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign in with Google</Button>
+                        <Button  style={{marginTop:"100%",marginRight:"0%"}} icon='google' color="white" size="large" onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign in with Google</Button>
                           )}
                           buttonText="Login"
-                          onSuccess={responseGoogle}
-                          onFailure={responseGoogle}
+                          onSuccess={this.responseGoogle}
+                          onFailure={this.responseGoogle}
                           cookiePolicy={'single_host_origin'}
                         />
                       </div>
